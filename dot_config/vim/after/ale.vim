57b5a605708349cf126440e0ab51b32eac0bc45a
bede <bede@devvm2095.vll0.facebook.com>
2021-04-16 13:19:26 -0700

    Initial

---
+++ dot_config/vim/after/ale.vim
@@ -0,0 +1,11 @@
+let g:ale_python_autopep8_use_global=1
+let g:ale_python_flake8_use_global=1
+let g:ale_python_mypy_use_global=1
+let g:ale_python_mypy_options='--ignore-missing-imports'
+let g:ale_python_pycodestyle_use_global=1
+let g:ale_python_pylint_use_global=1
+let g:ale_python_yapf_use_global=1
+let g:ale_python_flake8_executable = 'flake8-3'
+
+# ale-completion-completeopt-bug
+set completeopt=menu,menuone,popup,noselect,noinsert
---
+++ dot_config/vim/templates/skeleton.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash                                                                                                                                                                                                                    
+read -r -d '' doc <<"EOF"
+INSERT DOCO HERE
+
+Arguments:
+  -d|--debug = Enable debug mode, see commands but don't run (excludes passwords)
+  --clowntown = bypass are you sure prompts
+
+Usage:
+  <replace> <devices file> <config to push>
+  example_cmd ~/devices new_config
+EOF
+
+if [[ $# -eq 0 ]]; then
+    echo "$doc"
+    exit 1
+fi
+
+while :; do
+  case $1 in
+    --clowntown)
+      _clowntown_mode=1
+    ;;
+    -d|--debug)
+      _debug_mode=1
+    ;;
+    -?*)
+      echo "ERROR: Unknown argument"
+      echo "$doc"
+      exit 1
+    ;;
+    *)
+      break
+  esac
+  shift
+done
+
