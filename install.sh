9eea038ba4af83f4bae3bef753d5e2a5b1585801
Bede Carroll <4151340+bedecarroll@users.noreply.github.com>
2021-11-25 20:59:58 -0800

    Fix install script, chezmoi run after was unable to find bin

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 # Create bin dir and then make sure chezmoi is put there using -b
+PATH="$HOME/.local/bin:$PATH"
 mkdir -p ~/.local/bin/ && sh -c "$(curl -fsLS git.io/chezmoi)" -- -b ~/.local/bin/ init --apply bedecarroll
