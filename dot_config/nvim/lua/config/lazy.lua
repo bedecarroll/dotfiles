9e835e492d2e870f2b068c85cd8ac554ada33fd7
Bede Carroll <4151340+bedecarroll@users.noreply.github.com>
2024-08-29 08:22:14 -0700

    Prefer fzf over venv because that is usually auto

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -23,7 +23,8 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.coding.mini-comment" },
 		{ import = "lazyvim.plugins.extras.coding.mini-surround" },
 		{ import = "lazyvim.plugins.extras.coding.yanky" },
-		-- { import = "lazyvim.plugins.extras.editor.fzf" },
+		-- This breaks the venv selector plugin
+		{ import = "lazyvim.plugins.extras.editor.fzf" },
 		{ import = "lazyvim.plugins.extras.lang.ansible" },
 		{ import = "lazyvim.plugins.extras.lang.docker" },
 		{ import = "lazyvim.plugins.extras.lang.gleam" },
