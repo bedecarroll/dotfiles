f9aa33e74e9cf777631239a9905d44d83bac39a2
Bede Carroll <4151340+bedecarroll@users.noreply.github.com>
2024-11-30 21:01:42 -0800

    Fix missing fonts for waybar

--- nix/nixos-modules/fonts.nix
+++ nix/nixos-modules/fonts.nix
@@ -1,6 +1,15 @@
 { pkgs, ... }:
 {
   fonts.packages = with pkgs; [
+    # Waybar etc fonts
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    liberation_ttf
+    nerdfonts
+    roboto-mono
+    font-awesome
+    # Coding/terminal fonts
     fira-code
     fira-code-symbols
     comic-mono
