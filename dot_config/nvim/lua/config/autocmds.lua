1cf94a36d9eb783842b5c3c6004081940aea56dd
Bede Carroll <4151340+bedecarroll@users.noreply.github.com>
2025-05-03 11:59:06 -0700

    fix(nvim): fix comment continuation disable

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,16 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- https://github.com/dpetka2001/dotfiles/blob/43ecf1a6c1f1c8bf4ffe6fbc591f46db25b592db/dot_config/nvim/lua/config/autocmds.lua#L31-L44
+vim.api.nvim_create_augroup("mygroup", { clear = true })
+vim.api.nvim_create_autocmd("Filetype", {
+	pattern = { "*" },
+	callback = function()
+		vim.opt.formatoptions = vim.opt.formatoptions + {
+			o = false, -- Don't continue comments with o and O
+		}
+	end,
+	group = "mygroup",
+	desc = "Don't continue comments with o and O",
+})
