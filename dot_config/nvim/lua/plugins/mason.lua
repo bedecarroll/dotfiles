959e541b7ca8dda1ea2992de974759579f3ae475
Bede Carroll <4151340+bedecarroll@users.noreply.github.com>
2025-05-11 21:36:55 -0700

    fix(nvim): pin mason version until v2 support added to lazy

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -2,12 +2,17 @@
 return {
 	{
 		"williamboman/mason.nvim",
+		branch = "v1.x",
 		opts = {
 			ensure_installed = {
 				"bash-language-server",
 				"jq-lsp",
 				"jinja-lsp",
 			},
+		},
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+			branch = "v1.x",
 		},
 	},
 }
