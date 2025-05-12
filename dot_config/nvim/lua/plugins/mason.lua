e876d5ff4e516397ddc3b4e63417aa7b5560e231
Bede Carroll <4151340+bedecarroll@users.noreply.github.com>
2025-05-12 13:23:01 -0700

    fix(nvim): lazy now has pinned mason

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -1,18 +1,13 @@
 -- Additional Mason LSPs
 return {
 	{
-		"williamboman/mason.nvim",
-		branch = "v1.x",
+		"mason-org/mason.nvim",
 		opts = {
 			ensure_installed = {
 				"bash-language-server",
 				"jq-lsp",
 				"jinja-lsp",
 			},
-		},
-		dependencies = {
-			"williamboman/mason-lspconfig.nvim",
-			branch = "v1.x",
 		},
 	},
 }
